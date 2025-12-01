913afaaea495a63076bf085d66597ca0f98a7b04
Shaltiel Shmidman <shaltieltzion@gmail.com>
2025-12-01 18:24:40 +0200

    fixed missing func

---
+++ front-end/src/utils/analytics.js
@@ -0,0 +1,9 @@
+import ReactGA from 'react-ga4';
+
+export const initGA = () => {
+  const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
+  
+  if (measurementId) {
+    ReactGA.initialize(measurementId);
+  }
+};
